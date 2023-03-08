bfa0fd70ecc3a25f6ca458a43b76e2fe3b7c4e68
ingyukim <ikim59@bcit.ca>
2023-03-07 17:20:02 -0800

    make array of objects and do while loop

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,36 @@
+const courseList = [
+    {
+        code: "ACIT 1620",
+        name: "Fundamental Web Technologies"
+    },
+
+    {
+        code: "ACIT 1630",
+        name: "Database Systems"
+    },
+
+    {
+        code: "ACIT 1310",
+        name: "Technical Math for IT"
+    },
+
+    {
+        code: "ACIT 1100",
+        name: "Organizational Behaviour"
+    }
+];
+
+let result = 0;
+let user_input;
+let new_item;
+
+do {
+    user_input = prompt("Type fore digit number: ");
+    if (isNaN(user_input) === false && user_input.length === 4) {
+        result = 1;
+    }
+} while (result != 1);
+
+
 
 
